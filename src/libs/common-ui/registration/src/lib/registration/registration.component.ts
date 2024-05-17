6c393151d917e332078ce06e29a46dd182860517
thoaidz <zzaazzbadao@gmail.com>
2024-05-18 02:04:32 +0700

    create notification service and handle notification, auth service registration user

--- src/libs/common-ui/registration/src/lib/registration/registration.component.ts
+++ src/libs/common-ui/registration/src/lib/registration/registration.component.ts
@@ -1,11 +1,92 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  RegistrationFormProperties,
+  RegistrationFormValue,
+} from './model/registrationFormProperies';
+import { AuthenticationService } from '@school-master/services';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { finalize } from 'rxjs';
+import { NotificationService } from '@school-master/utilities/service';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss',
 })
-export class RegistrationComponent {}
+export class RegistrationComponent implements OnInit {
+  private formBuilder = inject(FormBuilder);
+  private authenticationService = inject(AuthenticationService);
+  private notificationService = inject(NotificationService);
+  private changeDetectorRef = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef);
+
+  registrationForm: FormGroup = new FormGroup({});
+
+  loading = false;
+
+  RegistrationFormProperties = RegistrationFormProperties;
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  private initForm() {
+    this.registrationForm = this.formBuilder.group({
+      [RegistrationFormProperties.FIRST_NAME]: ['', [Validators.required]],
+      [RegistrationFormProperties.LAST_NAME]: ['', [Validators.required]],
+      [RegistrationFormProperties.EMAIL]: ['', [Validators.required]],
+      [RegistrationFormProperties.PASSWORD]: ['', [Validators.required]],
+      [RegistrationFormProperties.RE_PASSWORD]: ['', [Validators.required]],
+      [RegistrationFormProperties.POST_CODE]: [''],
+      [RegistrationFormProperties.PHONE_NUMBER]: [''],
+    });
+  }
+
+  handleSignUp() {
+    const formData =
+      this.registrationForm.getRawValue() as RegistrationFormValue;
+    this.loading = true;
+    this.authenticationService
+      .signup({
+        firstName: formData[RegistrationFormProperties.FIRST_NAME],
+        lastName: formData[RegistrationFormProperties.LAST_NAME],
+        password: formData[RegistrationFormProperties.PASSWORD],
+        passwordConfirm: formData[RegistrationFormProperties.RE_PASSWORD],
+      })
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+          this.changeDetectorRef.markForCheck();
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe({
+        next: () => {
+          this.notificationService.setNotification({
+            message: 'Registration successfully created',
+          });
+        },
+        error: (err) => {
+          this.notificationService.setNotification({
+            modifier: 'danger',
+            message: err.message,
+          });
+        },
+      });
+  }
+}
---
+++ src/libs/utilities/service/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+import { CookieKey, User } from '@school-master/utilities/constants';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private _activeUser$ = new BehaviorSubject<User | undefined>(undefined);
+
+  constructor(private cookieService: CookieService) {
+    const activeUser = cookieService.get(CookieKey.ActiveUser);
+    if (activeUser) {
+      this._activeUser$.next(
+        JSON.parse(cookieService.get(CookieKey.ActiveUser))
+      );
+    }
+  }
+
+  // getActiveUser$(): BehaviorSubject<User | undefined> {
+  //   return this._activeUser$;
+  // }
+  //
+  // setActiveUser$(value: User | undefined) {
+  //   this._activeUser$.next(value);
+  // }
+}
