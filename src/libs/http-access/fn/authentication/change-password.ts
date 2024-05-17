f22ba5fb503be4ce63d04ded655d34a678007fce
thoaidz <zzaazzbadao@gmail.com>
2024-05-18 02:02:52 +0700

    update service

---
+++ src/libs/http-access/fn/authentication/change-password.ts
@@ -0,0 +1,40 @@
+/* tslint:disable */
+/* eslint-disable */
+import { HttpClient, HttpContext, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { StrictHttpResponse } from '../../strict-http-response';
+import { RequestBuilder } from '../../request-builder';
+
+export interface ChangePassword$Params {
+  body?: {
+    new_password1?: string;
+    new_password2?: string;
+    old_password?: string;
+  };
+}
+
+export function changePassword(
+  http: HttpClient,
+  rootUrl: string,
+  params?: ChangePassword$Params,
+  context?: HttpContext
+): Observable<StrictHttpResponse<void>> {
+  const rb = new RequestBuilder(rootUrl, changePassword.PATH, 'post');
+  if (params) {
+    rb.body('body', params.body, {});
+  }
+
+  return http
+    .request(rb.build({ responseType: 'text', accept: '*/*', context }))
+    .pipe(
+      filter((r: any): r is HttpResponse<any> => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return (r as HttpResponse<any>).clone({
+          body: undefined,
+        }) as StrictHttpResponse<void>;
+      })
+    );
+}
+
+changePassword.PATH = '/hr/password-change/';
